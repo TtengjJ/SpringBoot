c64cdfc3c51be4e3223a27d93dd95d577c7ab35b
TtengjJ <162894558+TtengjJ@users.noreply.github.com>
2025-05-20 13:21:42 +0800

    First

---
+++ SpringMCVWeb/demo/src/test/java/com/example/servingwebcontent/ServingWebContentApplicationTest.java
@@ -0,0 +1,15 @@
+
+//生成一个通用的测试
+package com.example.servingwebcontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServingWebContentApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
